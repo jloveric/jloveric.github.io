922e8686a71d00cbf4d84527e505fb03e20d221f
John Loverich <john.loverich@gmail.com>
2019-12-13 18:47:32 -0800

    Trying to cleanup.  Probably need to move to react-xterm if I can get it to work.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Terminal from 'terminal-in-react';
+//import {XTerm, Terminal} from 'react-xterm'
+
 import './App.css';
 
 function App() {
@@ -10,14 +12,24 @@ function App() {
           allowTabs='true'
           startState="maximised"
           color='green'
+          outputColor='gray'
           backgroundColor='black'
           barColor='black'
           style={{ fontWeight: "bold", fontSize: "1em", width: "100%" }}
           commandPassThrough={cmd => { 
             return `You said ${cmd.join(' ')}!  This will be connected to the client neural chat bot soon! Stay tuned.`
           }}
-          msg='MU-TH-UR 6000 ... Hello Lt. Ripley'
+          msg="John Loverich Bot - Under Construction"
         />
+
+        /*<XTerm ref='xterm' style={{
+          addons:['fit', 'fullscreen', 'search'],
+          overflow: 'hidden',
+          position: 'relative',
+          width: '100%',
+          height: '100%'
+        }} />*/
+        
       
     );
   

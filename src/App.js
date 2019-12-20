c903c1a9c1395834c646ca26ead9fcc38db6f9a8
John Loverich <john.loverich@gmail.com>
2019-12-19 20:00:48 -0800

    Adding a phrase database.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import NeuralChatbotReactRetro from 'neural-chatbot-react-retro'
-
+import defaultConfig from './PhraseDatabase';
 import './App.css';
 
 function App() {
 
   return (
     <div style={{ margin: '2%' }}>
-      <NeuralChatbotReactRetro backgroundColor='black'></NeuralChatbotReactRetro>
+      <NeuralChatbotReactRetro backgroundColor='black' botConfig={defaultConfig}></NeuralChatbotReactRetro>
     </div>
   );
 
